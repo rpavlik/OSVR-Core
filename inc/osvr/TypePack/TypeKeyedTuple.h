4f239840cf85b4fcc3c92cf33619158c221c9075
Ryan Pavlik <ryan@sensics.com>
2015-09-14 17:53:13 -0500

    Work on type keyed tuple
    All this works fine on GCC...

--- inc/osvr/TypePack/TypeKeyedTuple.h
+++ inc/osvr/TypePack/TypeKeyedTuple.h
@@ -35,7 +35,59 @@
 
 namespace osvr {
 namespace typepack {
+    template <typename KeyList, typename ComputeValueTypes>
+    class TypeKeyedTuple;
+    namespace detail {
+        template <typename Key, typename TypeKeyedContainer>
+        struct GetIndexInTypeKeyedImpl {
+            using key_types = typename TypeKeyedContainer::key_types;
+            static_assert(contains<key_types, Key>::value,
+                          "key type given not in the list of key types for "
+                          "this container");
+            using type = find_first<key_types, Key>;
+        };
+        template <typename Key, typename TypeKeyedContainer>
+        using GetIndexInTypeKeyed =
+            t_<GetIndexInTypeKeyedImpl<Key, TypeKeyedContainer>>;
 
+        template <typename TypeKeyedContainer>
+        struct TypeKeyedContainerStorageImpl;
+
+        template <typename KeyList, typename ComputeValueTypes>
+        struct TypeKeyedContainerStorageImpl<
+            TypeKeyedTuple<KeyList, ComputeValueTypes>> {
+            using value_types = transform<KeyList, ComputeValueTypes>;
+            using type = apply_list<quote<std::tuple>, value_types>;
+        };
+
+        template <typename TypeKeyedContainer>
+        using TypeKeyedContainerStorage =
+            t_<TypeKeyedContainerStorageImpl<TypeKeyedContainer>>;
+
+        template <typename Key, typename TypeKeyedContainer>
+        struct ValueAtTypeKeyedImpl;
+        template <typename Key, typename KeyList, typename ComputeValueTypes>
+        struct ValueAtTypeKeyedImpl<
+            Key, TypeKeyedTuple<KeyList, ComputeValueTypes>> {
+            using container = TypeKeyedTuple<KeyList, ComputeValueTypes>;
+            using tuple = TypeKeyedContainerStorage<container>;
+            using index = GetIndexInTypeKeyed<Key, container>;
+            using type = t_<std::tuple_element<index::value, tuple>>;
+        };
+        template <typename Key, typename TypeKeyedContainer>
+        using ValueAtTypeKeyed =
+            t_<ValueAtTypeKeyedImpl<Key, TypeKeyedContainer>>;
+        template <typename Key, typename TypeKeyedContainer>
+        using RefAtTypeKeyed = t_<std::add_lvalue_reference<
+            ValueAtTypeKeyed<Key, TypeKeyedContainer>>>;
+
+        template <typename Key, typename TypeKeyedContainer>
+        using ConstRefAtTypeKeyed = t_<std::add_lvalue_reference<
+            t_<std::add_const<ValueAtTypeKeyed<Key, TypeKeyedContainer>>>>>;
+        template <typename Key, typename TypeKeyedContainer>
+        using RRefAtTypeKeyed = t_<std::add_rvalue_reference<
+            ValueAtTypeKeyed<Key, TypeKeyedContainer>>>;
+    } // namespace detail
     /// @brief Provides a data structure where a value of heterogeneous data
     /// types may be stored at runtime for each of the "key" types in a list.
     /// The runtime data type stored is computed by an alias class.
@@ -50,52 +102,27 @@ namespace typepack {
     /// be constant)
     template <typename KeyList, typename ComputeValueTypes>
     class TypeKeyedTuple {
-        using value_types = transform<KeyList, ComputeValueTypes>;
+        using type = TypeKeyedTuple<KeyList, ComputeValueTypes>;
 
       public:
-        using container_type = apply_list<quote<std::tuple>, value_types>;
         using key_types = KeyList;
-
-        template <typename Key>
-        using value_type_for = apply<ComputeValueTypes, Key>;
-        template <typename Key>
-        using value_reference_for = value_type_for<Key> &;
-        template <typename Key>
-        using value_const_reference_for = value_type_for<Key> const &;
-        template <typename Key> using valid_key = contains<key_types, Key>;
-        template <typename Key> using index = find_first<key_types, Key>;
-
+        using container_type = detail::TypeKeyedContainerStorage<type>;
         /// @brief Accessor by key type - either cast nullptr to a Key const *,
         /// or explicitly specify a template argument.
         template <typename Key>
-        auto getElement(Key const * = nullptr) -> value_reference_for<Key> {
-            static_assert(valid_key<Key>::value,
-                          "Key type not found in the list!");
-            return std::get<index<Key>::value>(container_);
+        auto getElement(Key const * = nullptr)
+            -> detail::RefAtTypeKeyed<Key, type> {
+            using index = detail::GetIndexInTypeKeyed<Key, type>;
+            return std::get<index::value>(container_);
         }
 
         /// @brief Const accessor by key type - either cast nullptr to a Key
         /// const *, or explicitly specify a template argument.
         template <typename Key>
         auto getElement(Key const * = nullptr) const
-            -> value_const_reference_for<Key> {
-            static_assert(valid_key<Key>::value,
-                          "Key type not found in the list!");
-            return std::get<index<Key>::value>(container_);
-        }
-
-        /// @brief Subscript operator, if you don't mind instantiating your key
-        /// type.
-        template <typename Key>
-        auto operator[](Key && ) -> decltype(getElement<Key>()) {
-            return getElement<Key>();
-        }
-
-        /// @brief Const subscript operator, if you don't mind instantiating
-        /// your key type.
-        template <typename Key>
-        auto operator[](Key &&) const -> decltype(getElement<Key>()) {
-            return getElement<Key>();
+            -> detail::ConstRefAtTypeKeyed<Key, type> {
+            using index = detail::GetIndexInTypeKeyed<Key, type>;
+            return std::get<index::value>(container_);
         }
 
       private:
@@ -104,17 +131,17 @@ namespace typepack {
 
     /// @brief Nonmember accessor for typepack::TypeKeyedTuple - explicitly
     /// specify your key type.
-    template <typename Key, typename KeyList, typename ComputeValueTypes>
-    inline auto get(TypeKeyedTuple<KeyList, ComputeValueTypes> &c)
-        -> decltype(c.template getElement<Key>()) {
+    template <typename Key, typename KL, typename CVT>
+    inline detail::RefAtTypeKeyed<Key, TypeKeyedTuple<KL, CVT>>
+    get(TypeKeyedTuple<KL, CVT> &c) {
         return c.template getElement<Key>();
     }
 
     /// @brief Nonmember const accessor for typepack::TypeKeyedTuple -
     /// explicitly specify your key type.
-    template <typename Key, typename KeyList, typename ComputeValueTypes>
-    inline auto get(TypeKeyedTuple<KeyList, ComputeValueTypes> const &c)
-        -> decltype(c.template getElement<Key>()) {
+    template <typename Key, typename KL, typename CVT>
+    inline detail::ConstRefAtTypeKeyed<Key, TypeKeyedTuple<KL, CVT>>
+    get(const TypeKeyedTuple<KL, CVT> &c) {
         return c.template getElement<Key>();
     }
 } // namespace typepack
